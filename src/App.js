20f33729fae877616a95456aa5dab82e3dd56d3c
hyunjin-C <zn9852@naver.com>
2023-05-20 01:07:43 +0900

    feat: education & present page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Main from './page/Main';
+// import Main from './page/Main';
+import Present from './page/Present';
+import Education from './page/Education';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/main" element={<Main />} />
+                {/* <Route path="/main" element={<Main />} /> */}
+                <Route path="/present/:id" element={<Present />} />
+                <Route path="/education/:id" element={<Education />} />
             </Routes>
         </Router>
     );
---
+++ src/components/RemainingTime.jsx
@@ -0,0 +1,51 @@
+import { CiTimer } from 'react-icons/ci';
+
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
+import axios from 'axios';
+
+const RemainingTime = ({ id }) => {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const [remainingTime, setRemainingTime] = useState('');
+
+    useEffect(() => {
+        // 남은 시간을 서버에서 가져오는 함수
+        const fetchRemainingTime = async () => {
+            try {
+                const response = await axios.get(
+                    `${
+                        process.env.REACT_APP_SERVER_ADDR
+                    }/childrens/${params.get('id')}${[
+                        params.get('phone').slice(-4),
+                    ]}`,
+                );
+                setRemainingTime(response.educations[id].duration);
+            } catch (error) {
+                console.error(
+                    '남은 시간을 가져오는 중에 에러가 발생했습니다:',
+                    error,
+                );
+            }
+        };
+
+        fetchRemainingTime();
+    }, []);
+
+    return (
+        <div>
+            <CiTimer />
+            <p>남은 시간</p>
+            {remainingTime ? (
+                <p>
+                    {remainingTime.hours}:{remainingTime.minutes}:
+                    {remainingTime.seconds}
+                </p>
+            ) : (
+                <p>로딩 중입니다...</p>
+            )}
+        </div>
+    );
+};
+
+export default RemainingTime;
